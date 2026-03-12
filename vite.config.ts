2691f4d60d06fd2f4a272eb33c6e1d07ad3faae5
ronyw7 <yifengwang@berkeley.edu>
2026-03-11 23:10:35 -0700

    Add gh actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: process.env.BASE_URL || '/',
   plugins: [react(), tailwindcss()],
 })
